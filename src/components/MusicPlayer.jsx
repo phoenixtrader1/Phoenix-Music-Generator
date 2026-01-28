eee22321a8d06dcc3865df8f63622f7ec8e7868a
phoenixtrader1 <sarmientomarc3@gmail.com>
2026-01-28 20:49:50 +0800

    Create MusicPlayer.jsx

---
+++ src/components/MusicPlayer.jsx
@@ -0,0 +1,11 @@
+export default function MusicPlayer({ audioSrc }) {
+  return (
+    <div style={{ marginTop: "1rem" }}>
+      <audio controls src={audioSrc}></audio>
+      <br />
+      <a href={audioSrc} download="music.wav">
+        Download
+      </a>
+    </div>
+  );
+}
